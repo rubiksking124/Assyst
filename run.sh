d8898535f9ec85a60d2de61477624943bcd179e9
Timo <30553356+y21@users.noreply.github.com>
2020-02-11 20:29:04 +0100

    shell script

---
+++ run.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+while :
+do
+        echo "Starting process..."
+        node /home/assyst/JS/index.js
+        if [ $? -eq 0 ]
+        then
+                echo "Stopping process..." # exit shell script if exit code is 0
+                break
+        fi
+        sleep 10 # wait 10 seconds before restarting
+done
