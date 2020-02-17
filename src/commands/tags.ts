b7b24d685ebb07898fcc38293608fd70e46dbfe4
Timo <btimo8822@gmail.com>
2020-02-17 19:22:39 +0100

    add paginator

--- src/commands/tags.ts
+++ src/commands/tags.ts
@@ -1,6 +1,6 @@
 import Command from '../../lib/Command';
 import Assyst from '../../lib/Assyst';
-import { COOLDOWN_TYPES, MESSAGE_TYPE_EMOTES } from '../../lib/Enums';
+import { COOLDOWN_TYPES, MESSAGE_TYPE_EMOTES, PERMISSION_LEVELS } from '../../lib/Enums';
 import { ICommandContext } from '../../lib/CInterfaces';
 import { Message } from 'detritus-client/lib/structures';
 
@@ -14,21 +14,32 @@ export default class Tags extends Command {
                 timeout: 5000,
                 type: COOLDOWN_TYPES.GUILD
             },
-            validFlags: [],
+            validFlags: [{
+                name: 'mine',
+                description: 'Get the list of tags that you own in this guild',
+                permissionLevel: PERMISSION_LEVELS.NORMAL,
+                argumented: false
+            }],
             info: {
                 description: 'Get information about the tags in the current guild',
                 examples: [],
-                usage: "",
-                author: "Jacherr"
+                usage: '',
+                author: 'Jacherr'
             }
         });
     }
 
     public async execute(context: ICommandContext): Promise<Message | null> {
-        let guildTags: { name: string, author: string, uses: number }[] = await this.assyst.sql('select name, author, uses from tags where guild = $1', [context.message.channel?.guild?.id]).then(r => r.rows)
-        const totalTags: number = guildTags.length
-        guildTags = guildTags.sort((a, b) => b.uses - a.uses).slice(0, 9)
-        if(guildTags.length === 0) {
+        let tags: any[];
+        if (context.checkForFlag("mine")) {
+            tags = await this.assyst.sql('select * from tags where guild = $1 and author = $2', [context.message.guildId, context.message.author.id])
+                .then(v => v.rows);
+        } else {
+            tags = await this.assyst.sql('select * from tags where guild = $1', [context.message.guildId])
+                .then(v => v.rows);
+        }
+
+        if (tags.length === 0) {
             return context.reply('This guild has no tags yet.', {
                 storeAsResponseForUser: {
                     user: context.message.author.id,
@@ -37,16 +48,30 @@ export default class Tags extends Command {
                 type: MESSAGE_TYPE_EMOTES.INFO
             })
         }
-        let fields: { name: string, value: string, inline: boolean }[] = guildTags.map(t => {
-            return { name: t.name, value: `**Owner:** ${this.bot.users.get(t.author) ? this.bot.users.get(t.author) : `${t.author} (not in guild)`}\n**Uses:** ${t.uses.toString()}`, inline: true}
-        })
-        return context.reply({embed: {
-            title: `Guild tags - ${context.message.channel?.guild?.name}`,
-            fields,
-            color: this.assyst.embedColour,
-            footer: {
-                text: `Total guild tags: ${totalTags}`
-            }
-        }})
+
+        const pages = [];
+        for (let i = 0; i < tags.length; i += 10) {
+            pages.push({
+                embed: {
+                    name: `Guild tags - ${context.message.guild?.name}`,
+                    fields: tags.slice(i, i + 10).map((tag: any) => ({
+                        name: tag.name || "?",
+                        value: `**Owner: ** ${this.bot.users.get(tag.author) ? this.bot.users.get(tag.author) : `${tag.author} (not in this server)`}\n` +
+                               `Uses: ${tag.uses}`,
+                        inline: true
+                    })),
+                    color: this.assyst.embedColour,
+                    footer: {
+                        text: `Total guild tags: ${tags.length} | Page: ${(i / 10) + 1}`
+                    }
+                }
+            });
+        }
+
+        const paginator = await this.assyst.paginator.createReactionPaginator({
+            message: context.message,
+            pages
+        });
+        return paginator.commandMessage;
     }
 }
