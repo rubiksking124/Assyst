a5faf0440aaac69c859de84343dfd973f7372e42
Timo <btimo8822@gmail.com>
2020-02-28 20:42:47 +0100

    better eslint

--- index.ts
+++ index.ts
@@ -1,17 +1,16 @@
 import Assyst from './lib/Assyst';
 import Config from './lib/Config';
-import { readdirSync } from 'fs';
 
 const client: Assyst = new Assyst({
     config: new Config()
 });
 
-client.bot.on("gatewayReady", () => {
-    console.info("Assyst is ready");
+client.bot.on('gatewayReady', () => {
+    console.info('Assyst is ready');
 });
 
 process.on('unhandledRejection', (err: any) => {
-    client.bot.channels.get(client.errorChannel)?.createMessage(`\`\`\`js\n${err.stack.toString()}\`\`\``)
+    client.bot.channels.get(client.errorChannel)?.createMessage(`\`\`\`js\n${err.stack.toString()}\`\`\``);
 });
 
 (async () => {
