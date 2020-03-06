717f3b49d2c3d2aebc6a96d7d46b86277484eb63
Jacher <jwc2002@outlook.com>
2020-03-06 21:04:49 +0000

    re-enable loadallmembers

--- lib/Assyst.ts
+++ lib/Assyst.ts
@@ -63,6 +63,7 @@ export default class Assyst {
                 identifyProperties: {
                     $browser: 'Discord iOS'
                 },
+                loadAllMembers: true
             },
         });
 
