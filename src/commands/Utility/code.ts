2a427559a18f0b7683e094c08cb51fec87b91c40
Jacher <jwc2002@outlook.com>
2020-04-08 19:52:41 +0100

    same

--- src/commands/Utility/code.ts
+++ src/commands/Utility/code.ts
@@ -4,6 +4,7 @@ import Assyst from '../../structures/Assyst';
 import { Markup } from 'detritus-client/lib/utils';
 
 import { STATUS_CODES } from 'http';
+import GocodeitRestClient from '../../rest/clients/Gocodeit';
 
 export default {
   name: 'code',
@@ -26,16 +27,18 @@ export default {
     }
   ],
   run: async (assyst: Assyst, ctx: Context, args: any) => {
+    const client: GocodeitRestClient | undefined = <GocodeitRestClient | undefined>assyst.customRest.clients.get('gocodeit');
+    if (!client) throw new Error('No gocodeit client is defined');
     if (!args || !args.code) {
       return ctx.editOrReply('You need to provide language and code arguments');
     } else if (args.code === 'list') {
-      const languages = await assyst.utils.getLanguageList().then((res) => res.data);
+      const languages = await client.getLanguageList().then((res) => res.data);
       return ctx.editOrReply(`Supported languages: ${languages.map(l => `\`${l}\``).join(', ')}`);
     } else if (args.code.split(' ').length === 1) {
       return ctx.editOrReply('You need to provide a code argument');
     }
     await ctx.triggerTyping();
-    const response = await assyst.utils.runSandboxedCode(args.code.split(' ')[0], args.code.split(' ').slice(1).join(' ')).then((res) => res);
+    const response = await client.runSandboxedCode(args.code.split(' ')[0], args.code.split(' ').slice(1).join(' ')).then((res) => res);
     if (response.status !== 200) {
       return ctx.editOrReply(`Error ${response.status}: ${STATUS_CODES[response.status]} - ${response.data.res}`);
     }
--- src/structures/Logger.ts
+++ src/structures/Logger.ts
@@ -8,4 +8,8 @@ export default class Logger {
     public info (content: string) {
       console.info(`${'='.repeat(this.spacing)} [INFO] ${content}`);
     }
+
+    public warn (content: string) {
+      console.warn(`${'='.repeat(this.spacing)} [WARN] ${content}`);
+    }
 }
