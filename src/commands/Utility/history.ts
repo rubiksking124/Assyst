49057e2a5daa4b8a06904aefe4de00b60f469929
Jacherr <jwc2002@outlook.com>
2020-04-16 00:58:43 +0100

    history command

---
+++ src/commands/Utility/history.ts
@@ -0,0 +1,28 @@
+import { Context } from 'detritus-client/lib/command';
+
+import Assyst from '../../structures/Assyst';
+import { Markup } from 'detritus-client/lib/utils';
+
+export default {
+  name: 'history',
+  aliases: ['onthisday', 'otd'],
+  responseOptional: true,
+  metadata: {
+    description: 'Get a random historical event that took place today',
+    usage: '',
+    examples: ['']
+  },
+  ratelimit: {
+    type: 'guild',
+    limit: 1,
+    duration: 5000
+  },
+  run: async (assyst: Assyst, ctx: Context, _args: any) => {
+    ctx.triggerTyping();
+    const month = new Date().getMonth() + 1
+    const day = new Date().getDate()
+    const response = await ctx.rest.request(`https://byabbe.se/on-this-day/${month}/${day}/events.json`).then(res => res.events)
+    let event = response[Math.floor(Math.random() * response.length)];
+    return ctx.editOrReply(`**On this day (${day}/${month}/${new Date().getFullYear()}):**\n${Markup.codeblock(event.description)}`);
+  }
+};
