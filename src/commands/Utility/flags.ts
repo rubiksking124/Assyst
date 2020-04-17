49312d1d6fd56fa8cd8f054a50f0caed3b15d0f5
Jacher <jwc2002@outlook.com>
2020-04-17 13:24:08 +0100

    add verified bot, verified bot developer, accept id as param

--- src/commands/Utility/flags.ts
+++ src/commands/Utility/flags.ts
@@ -20,7 +20,11 @@ export default {
     duration: 3000
   },
   run: async (assyst: Assyst, ctx: Context, args: any) => {
-    const user = ctx.user;
+    let user = ctx.user;
+    if (args && args.publicflags) {
+      user = await ctx.rest.fetchUser(args.publicflags);
+      if (!user) return ctx.editOrReply('No user found with this id');
+    }
     const publicFlags: Array<string> = [];
     for (let i = 0; i < 16; i++) {
       const flagName: string | undefined = UserFlags[1 << i];
@@ -28,6 +32,8 @@ export default {
         publicFlags.push(`${flagName.slice(0, 1)}${flagName.slice(1).toLowerCase()}: ${(user.publicFlags & 1 << i) !== 0}`);
       }
     }
+    publicFlags.push(`Verified_bot_developer: ${user.hasVerifiedDeveloper}`);
+    publicFlags.push(`Verified_bot: ${user.hasVerifiedBot}`);
     ctx.editOrReply(Markup.codeblock(publicFlags.join('\n'), { language: 'ml' }));
   }
 };
