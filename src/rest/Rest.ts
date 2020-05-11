cf013210cfa6762c25dceade212b21ab80ecda6d
Jacherr <jwc2002@outlook.com>
2020-05-12 00:07:23 +0100

    minor timeout increase on ss

--- src/rest/Rest.ts
+++ src/rest/Rest.ts
@@ -140,7 +140,7 @@ export default class RestController {
           'content-type': 'application/json'
         },
         settings: {
-          timeout: 30000
+          timeout: 32000
         },
         body: {
           args: {
