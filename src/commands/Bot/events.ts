b46a6493a115e1c165b2e0d2a44dd1baf0a2266d
Jacherr <jwc2002@outlook.com>
2020-05-18 00:43:39 +0100

    add total events

--- src/commands/Bot/events.ts
+++ src/commands/Bot/events.ts
@@ -20,6 +20,7 @@ export default {
   run: async (assyst: Assyst, ctx: Context, args: any) => {
     const events = await assyst.db.getEvents();
     const fields: MetricItem[] = [];
+    const totalEvents = events.map(e => parseInt(e.amount)).reduce((a, b) => a + b);
     events.sort((a, b) => parseInt(b.amount) - parseInt(a.amount)).forEach(event => {
       fields.push({
         name: event.name,
@@ -28,7 +29,7 @@ export default {
     });
     return ctx.editOrReply({
       embed: {
-        title: 'Event metrics since 18/05/20',
+        title: `Event metrics since 18/05/20 (Total ${totalEvents})`,
         description: Markup.codeblock(assyst.utils.formatMetricList(fields), { language: 'ml', limit: 1990 }),
         color: 0xf4632e,
       }
