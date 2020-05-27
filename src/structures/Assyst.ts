ffc48d3b082e0abe7ca0a43990ab3e79adfec111
Jacher <jwc2002@outlook.com>
2020-05-27 10:46:30 +0100

    lint, increase error webhook errors depth

--- src/structures/Assyst.ts
+++ src/structures/Assyst.ts
@@ -226,7 +226,7 @@ export default class Assyst extends CommandClient {
     if (error.errors) {
       extraFields.push({
         name: 'Errors',
-        value: Markup.codeblock(inspect(error.errors, { depth: 6, showHidden: true }))
+        value: Markup.codeblock(inspect(error.errors, { depth: 9, showHidden: true }))
       });
     }
     this.client.rest.executeWebhook(id, token, {
