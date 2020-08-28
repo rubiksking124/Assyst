053460a95c85768489f363f403d7b2e0e80fa587
Jacher <jwc2002@outlook.com>
2020-08-28 20:09:52 +0100

    remove dist

--- dist/src/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EmbedColors = void 0;
-var EmbedColors;
-(function (EmbedColors) {
-    EmbedColors[EmbedColors["ERROR"] = 11868436] = "ERROR";
-})(EmbedColors = exports.EmbedColors || (exports.EmbedColors = {}));
--- dist/src/rest/zx8.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Zx8 = void 0;
-class Zx8 {
-}
-exports.Zx8 = Zx8;
